ff1238dc03509bd596c105d8f1051ee03af2cbb3
Thanh Duc Nguyen <thanhduc.nguyen@optimizely.com>
2024-02-23 14:56:33 +0700

    Singleton

--- Singleton/Implementation.cs
+++ Singleton/Implementation.cs
@@ -1,4 +1,24 @@
 ﻿namespace Singleton
 {
-  
+    public class Database
+    {
+        private static readonly Lazy<Database> _instance = new Lazy<Database>(() => new Database());
+
+        private Database()
+        {
+        }
+
+        public static Database Instance
+        {
+            get
+            {
+                return _instance.Value;
+            }
+        }
+
+        public string Query(string query)
+        {
+            return "Data";
+        }
+    }
 }
--- Singleton/Program.cs
+++ Singleton/Program.cs
@@ -1,1 +1,7 @@
-﻿Console.Title = "Singleton";
+﻿using Singleton;
+
+Console.Title = "Singleton";
+
+var db = Database.Instance;
+var result = db.Query("SELECT * FROM something");
+Console.WriteLine(result);
