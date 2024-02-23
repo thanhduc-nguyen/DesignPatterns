e56da1718b4e4e2016f0ae29cc6b8bf52e769154
Thanh Duc Nguyen <thanhduc.nguyen@optimizely.com>
2024-02-23 16:35:18 +0700

    Factory Method

--- FactoryMethod/Implementation.cs
+++ FactoryMethod/Implementation.cs
@@ -1,4 +1,76 @@
 ﻿namespace FactoryMethod
 {
-   
+    /// <summary>
+    /// Product.
+    /// </summary>
+    public interface ITransport
+    {
+        public void Deliver();
+    }
+
+    /// <summary>
+    /// Concrete Product.
+    /// </summary>
+    public class Truck : ITransport
+    {
+        public int Id { get; set; } = Random.Shared.Next(100);
+
+        public void Deliver()
+        {
+            Console.WriteLine("Deliver by land in a box.");
+        }
+    }
+
+    /// <summary>
+    /// Concrete Product.
+    /// </summary>
+    public class Ship : ITransport
+    {
+        public int Id { get; set; } = Random.Shared.Next(100);
+
+        public void Deliver()
+        {
+            Console.WriteLine("Deliver by sea in a container.");
+        }
+    }
+
+    /// <summary>
+    /// Creator.
+    /// </summary>
+    public abstract class Logistics
+    {
+        /// <summary>
+        /// Factory Method.
+        /// </summary>
+        public abstract ITransport CreateProduct();
+
+        /// <summary>
+        /// Another operation of Logistics.
+        /// </summary>
+        public void SomeOperation()
+        {
+        }
+    }
+
+    /// <summary>
+    /// Concrete Creator A.
+    /// </summary>
+    public class RoadLogistics : Logistics
+    {
+        public override Truck CreateProduct()
+        {
+            return new Truck();
+        }
+    }
+
+    /// <summary>
+    /// Concrete Creator B.
+    /// </summary>
+    public class SeaLogistics : Logistics
+    {
+        public override Ship CreateProduct()
+        {
+            return new Ship();
+        }
+    }
 }
--- FactoryMethod/Program.cs
+++ FactoryMethod/Program.cs
@@ -1,1 +1,29 @@
-﻿Console.Title = "Factory Method";
+﻿using FactoryMethod;
+
+Console.Title = "Factory Method";
+
+var logistics = "Sea";
+var roadLogistics = new RoadLogistics();
+var seaLogistics = new SeaLogistics();
+var transports = new List<ITransport>();
+
+if (logistics == "Road")
+{
+    for (int i = 0; i < 10; i++)
+    {
+        transports.Add(roadLogistics.CreateProduct());
+    }
+}
+else
+{
+    for (int i = 0; i < 10; i++)
+    {
+        transports.Add(seaLogistics.CreateProduct());
+    }
+}
+
+transports.ForEach((t) =>
+{
+    Console.WriteLine($"{t.GetType().Name} {t.GetType().GetProperty("Id")!.GetValue(t)}");
+    t.Deliver();
+});
