0f853edd5083f3d48b61566cb10fa0318bd857d9
Thanh Duc Nguyen <thanhduc.nguyen@optimizely.com>
2024-02-29 17:35:30 +0700

    Observer

--- Observer/Implementation.cs
+++ Observer/Implementation.cs
@@ -1,3 +1,92 @@
-﻿namespace Observer
+﻿using System.Collections;
+
+namespace Observer
 {
+    /// <summary>
+    /// Subscriber.
+    /// </summary>
+    public interface IBroadcastChannel
+    {
+        void SendMessage(string data);
+    }
+
+    /// <summary>
+    /// Concrete Subscriber 1.
+    /// </summary>
+    public class Sms : IBroadcastChannel
+    {
+        public void SendMessage(string data)
+        {
+            Console.WriteLine($"SMS: {data}");
+        }
+    }
+
+    /// <summary>
+    /// Concrete Subscriber 2.
+    /// </summary>
+    public class Email : IBroadcastChannel
+    {
+        public void SendMessage(string data)
+        {
+            Console.WriteLine($"Email: {data}");
+        }
+    }
+
+    /// <summary>
+    /// Concrete Subscriber 3.
+    /// </summary>
+    public class Television : IBroadcastChannel
+    {
+        public void SendMessage(string data)
+        {
+            Console.WriteLine($"Television: {data}");
+        }
+    }
+
+    /// <summary>
+    /// Publisher.
+    /// </summary>
+    public class BroadcastManager
+    {
+        protected List<Tuple<string, IBroadcastChannel>> broadcastChannels = new List<Tuple<string, IBroadcastChannel>>();
+
+        public void Subscribe(string eventType, IBroadcastChannel channel)
+        {
+            broadcastChannels.Add(new Tuple<string, IBroadcastChannel>(eventType, channel));
+        }
+
+        public void Unsubscribe(string eventType, IBroadcastChannel channel)
+        {
+            broadcastChannels.RemoveAll(x => x.Item1.Equals(eventType) && x.Item2.GetType().Equals(channel.GetType()));
+        }
+
+        public void Notify(string eventType, string data)
+        {
+            var filteredBroadcastChannels = broadcastChannels.Where(x => x.Item1.Equals(eventType)).Select(x => x.Item2);
+            foreach (IBroadcastChannel channel in filteredBroadcastChannels)
+            {
+                channel.SendMessage(data);
+            }
+        }
+    }
+
+    public class Store
+    {
+        public BroadcastManager broadcastManager;
+
+        public Store(BroadcastManager broadcastManager)
+        {
+            this.broadcastManager = broadcastManager;
+        }
+
+        public void IntroduceNewPhone()
+        {
+            broadcastManager.Notify("phone", "A new phone was introduced!");
+        }
+
+        public void IntroduceNewLaptop()
+        {
+            broadcastManager.Notify("laptop", "A new laptop was introduced!");
+        }
+    }
 }
--- Observer/Program.cs
+++ Observer/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.Title = "Observer";
+﻿using Observer;
+
+Console.Title = "Observer";
+
+var store = new Store(new BroadcastManager());
+
+var sms = new Sms();
+var email = new Email();
+var television = new Television();
+
+Console.WriteLine("Add sms, email subscriber for New Phone Event and");
+Console.WriteLine("Add sms, email, television subscriber for New Laptop Event");
+store.broadcastManager.Subscribe("phone", sms);
+store.broadcastManager.Subscribe("phone", email);
+store.broadcastManager.Subscribe("laptop", sms);
+store.broadcastManager.Subscribe("laptop", email);
+store.broadcastManager.Subscribe("laptop", television);
+
+store.IntroduceNewPhone();
+store.IntroduceNewLaptop();
+
+Console.WriteLine("\nRemove sms subscriber for New Phone Event");
+store.broadcastManager.Unsubscribe("phone", sms);
+store.IntroduceNewPhone();
