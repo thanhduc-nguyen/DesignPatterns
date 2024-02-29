d71dc26af02c0c0b6f36d137503da497d9d5585b
Thanh Duc Nguyen <thanhduc.nguyen@optimizely.com>
2024-02-29 16:40:10 +0700

    Bridge

--- Bridge/Implementation.cs
+++ Bridge/Implementation.cs
@@ -1,3 +1,77 @@
 ﻿namespace Bridge
 {
+    /// <summary>
+    /// Abstraction.
+    /// </summary>
+    public abstract class Shape
+    {
+        protected IColor _color;
+        public abstract void Paint();
+    }
+
+    /// <summary>
+    /// Implementation.
+    /// </summary>
+    public interface IColor
+    {
+        string Paint();
+    }
+
+    /// <summary>
+    /// Refinded Abstraction 1.
+    /// </summary>
+    public class Circle : Shape
+    {
+        public Circle(IColor color)
+        {
+            _color = color;
+        }
+
+        public string Color { get; set; }
+
+        public override void Paint()
+        {
+            Color = _color.Paint();
+        }
+    }
+
+    /// <summary>
+    /// Refinded Abstraction 2.
+    /// </summary>
+    public class Square : Shape
+    {
+        public Square(IColor color)
+        {
+            _color = color;
+        }
+
+        public string Color { get; set; }
+
+        public override void Paint()
+        {
+            Color = _color.Paint();
+        }
+    }
+
+    /// <summary>
+    /// Concreate Implementation 1.
+    /// </summary>
+    public class RedColor : IColor
+    {
+        public string Paint()
+        {
+            return "Red";
+        }
+    }
+
+    /// <summary>
+    /// Concreate Implementation 2.
+    /// </summary>
+    public class BlueColor : IColor
+    {
+        public string Paint()
+        {
+            return "Blue";
+        }
+    }
 }
--- Bridge/Program.cs
+++ Bridge/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.Title = "Bridge";
+﻿using Bridge;
+
+Console.Title = "Bridge";
+
+var redColor = new RedColor();
+var blueColor = new BlueColor();
+
+var redCircle = new Circle(redColor);
+redCircle.Paint();
+Console.WriteLine($"redCircle is the {redCircle.GetType().Name} which is {redCircle.Color}");
+
+var redSquare = new Square(redColor);
+redSquare.Paint();
+Console.WriteLine($"redSquare is the {redSquare.GetType().Name} which is {redSquare.Color}");
+
+var blueCircle = new Circle(blueColor);
+blueCircle.Paint();
+Console.WriteLine($"blueCircle is the {blueCircle.GetType().Name} which is {blueCircle.Color}");
+
+var blueSquare = new Circle(blueColor);
+blueSquare.Paint();
+Console.WriteLine($"blueSquare is the {blueSquare.GetType().Name} which is {blueSquare.Color}");
