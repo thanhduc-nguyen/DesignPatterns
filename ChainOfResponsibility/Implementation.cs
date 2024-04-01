e247840751b1e322a4236fd1cff96e1c4405070d
Thanh Duc Nguyen <thanhduc.nguyen@episerver.com>
2024-04-01 17:37:52 +0700

    Chain Of Responsibility

--- ChainOfResponsibility/Implementation.cs
+++ ChainOfResponsibility/Implementation.cs
@@ -1,3 +1,82 @@
 ﻿namespace ChainOfResponsibility
 {
+    /// <summary>
+    /// Handler.
+    /// </summary>
+    public interface IHandler<TIn, TOut>
+    {
+        int Order { get; }
+        void SetNext(IHandler<TIn, TOut> handler);
+        TOut Handle(TIn input, TOut result);
+    }
+
+    /// <summary>
+    /// Base Handler.
+    /// </summary>
+    public abstract class BaseHandler<TIn, TOut> : IHandler<TIn, TOut>
+    {
+        protected IHandler<TIn, TOut>? next;
+        public abstract int Order { get; }
+
+        public void SetNext(IHandler<TIn, TOut> next)
+        {
+            this.next = next;
+        }
+
+        public abstract TOut Handle(TIn info, TOut result);
+    }
+
+    public class Info
+    {
+        public string Information { get; set; }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Gender { get; set; }
+    }
+
+    /// <summary>
+    /// Concrete Class.
+    /// </summary>
+    public class AddId : BaseHandler<Person, Info>
+    {
+        public override int Order => 1;
+
+        public override Info Handle(Person person, Info result)
+        {
+            result.Information += $"{person.Id} ";
+            return next != null ? next.Handle(person, result) : new Info();
+        }
+    }
+
+    /// <summary>
+    /// Concrete Class.
+    /// </summary>
+    public class AddName : BaseHandler<Person, Info>
+    {
+        public override int Order => 2;
+
+        public override Info Handle(Person person, Info result)
+        {
+            result.Information += $"{person.Name} ";
+            return next != null ? next.Handle(person, result) : new Info();
+        }
+    }
+
+    /// <summary>
+    /// Concrete Class.
+    /// </summary>
+    public class AddGender : BaseHandler<Person, Info>
+    {
+        public override int Order => 3;
+
+        public override Info Handle(Person person, Info result)
+        {
+            result.Information += $"{person.Gender} ";
+            return next != null ? next.Handle(person, result) : new Info();
+        }
+    }
 }
--- ChainOfResponsibility/Program.cs
+++ ChainOfResponsibility/Program.cs
@@ -1,1 +1,36 @@
-﻿Console.Title = "Chain of Responsibility";
+﻿using ChainOfResponsibility;
+
+Console.Title = "Chain of Responsibility";
+
+var handlers = new List<IHandler<Person, Info>>
+{
+    new AddName(),
+    new AddId(),
+    new AddGender()
+}.OrderBy(x => x.Order).ToList();
+
+var person = new Person
+{
+    Id = 1,
+    Name = "John",
+    Gender = "Male"
+};
+var info = new Info();
+
+var prevHandler = default(IHandler<Person, Info>);
+foreach (var handler in handlers)
+{
+    if (prevHandler == null)
+    {
+        prevHandler = handler;
+    }
+    else
+    {
+        prevHandler.SetNext(handler);
+        prevHandler = handler;
+    }
+}
+
+handlers.First().Handle(person, info);
+
+Console.WriteLine(info.Information);
