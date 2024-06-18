9ff27d5efaf41e22f01101622356795966c4ea5b
Thanh Duc Nguyen <thanhduc.nguyen@episerver.com>
2024-06-18 15:19:31 +0700

    clean up code

--- AbstractFactory/HeadFirst/Ingredients/Concretes/Eggplant.cs
+++ AbstractFactory/HeadFirst/Ingredients/Concretes/Eggplant.cs
@@ -1,9 +1,4 @@
 ﻿using AbstractFactory.HeadFirst.Ingredients.Abstracts;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace AbstractFactory.HeadFirst.Ingredients.Concretes
 {
